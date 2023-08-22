3c87f2c8fe5dc84cdab42687ded8b95a8e8621d4
mateusSpanjos <142438810+mateusSpanjos@users.noreply.github.com>
2023-08-22 17:23:00 -0300

    Create exercicio.js

---
+++ exercicio.js
@@ -0,0 +1,48 @@
+/*
+*Mateus Anjos
+*
+*/
+
+/*
+* 1.resposta 
+10
+10, 5
+
+* 2. reposta
+10, 20, 10
+
+* 3. resposta
+let p = prompt("Quantas horas você trabalha por dia?")
+let t = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe $(horasDinheiro) por hora`)
+
+*/
+//exercicio 3
+
+let nome
+let idade
+console.log(typeof nome)
+console.log (typeof idade)
+// porque nao foi atribuido nenhum valor para as variaveis
+//let nome = "mateus";
+//let idade = "15";
+nome = prompt ("Qual e o seu nome?");
+idade = prompt ("Qual e a sua idade?");
+
+console.log ("Ola", nome, "voce tem",  idade, "anos");
+
+let resposta;
+let resposta2;
+let resposta3;
+resposta = prompt ("Voce joga video game?");
+console.log (typeof resposta);
+resposta2 = prompt ("Voce gosta de cachorros?");
+console.log (typeof resposta2);
+resposta3 = prompt ("Voce ouve musica?")
+console.log (typeof resposta3)
+console.log (resposta, "Voce joga video game?");
+console.log (resposta2, "Voce gosta de cachorros?");
+console.log (resposta3, "Voce ouve musica?");
+
+
+
